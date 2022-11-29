3a9dd4c6cc4cf5733cf9c23fd0ca03396bb08eec
Omprasad143 <117642999+Omprasad143@users.noreply.github.com>
2022-11-29 16:29:46 +0530

    Update index.js

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -5,23 +5,24 @@ import './index.css'
 class Counter extends Component {
   state = {count: 0}
   onIncrement = () => {
-      this.state(prevState => {
+      this.setState(prevState => {
            console.log(`previous state value ${prevState.count}`)
-           return (count: prevState.count + 1)
+           return {count: prevState.count + 1}
       })
    
   }
 
   onDecrement = () => {
-      this.state(prevState => {
+      this.setState(prevState => {
           console.log(`previous state value ${prevState.count}`)
-          return (count: prevState.count - 1)
+          return {count: prevState.count - 1}
       })
    
   }
 
   render() {
       const {count} =this.state
+      return (
     <div>
       <h1 className="heading">Counter</h1>
       <p class="count">{count}</p>
@@ -35,5 +36,8 @@ class Counter extends Component {
         </button>
       </div>
     </div>
+)
   }
 }
+
+export default Counter
